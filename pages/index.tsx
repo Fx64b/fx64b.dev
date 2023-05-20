140051325f191da56513665fb2333958c01f123a
Fx64b <90456762+Fx64b@users.noreply.github.com>
2023-05-20 17:39:44 +0200

    fix: title

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Fx64b</title>
         <meta name="description" content="Fx64b.dev website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
