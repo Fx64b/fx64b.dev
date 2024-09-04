9ffb92cd46fa39dc723dfbd8496d3e9015714a61
Fx64b <90456762+Fx64b@users.noreply.github.com>
2024-09-04 07:23:21 +0200

    feat: add backgroundcolor, logo and basic content

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-import { CircularProgress, Link } from '@nextui-org/react'
+import { Image } from '@nextui-org/image'
 
 export default function Home() {
     return (
-        <div className="flex h-screen w-screen items-center justify-center">
-            <div className="flex flex-col items-center gap-y-4">
-                <b>
-                    Wait a second,{' '}
-                    <Link href={'https://github.com/Fx64b'}>F_x64b</Link> is
-                    working on it.{' '}
-                </b>
-                <CircularProgress size="lg" aria-label="Loading..." />
+        <div className="flex h-screen w-screen justify-center bg-background">
+            <div className="mt-10 flex flex-col items-center gap-y-4 px-4 text-center">
+                <Image
+                    className={'w-48 rounded-full border-2 border-white'}
+                    src="/logo.svg"
+                    alt={
+                        'Fx64b profile picture displaying the letter F in a serif font'
+                    }
+                />
+                <h1 className={'text-2xl font-bold'}>Fx64b</h1>
+                <p>
+                    Lorem ipsum odor amet, consectetuer adipiscing elit.
+                    Habitasse quam venenatis tempus hendrerit lorem.
+                </p>
             </div>
         </div>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,15 @@ const config: Config = {
         './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                background: {
+                    light: '#030709',
+                    dark: '#030709',
+                    DEFAULT: '#030709',
+                },
+            },
+        },
     },
     darkMode: 'class',
     plugins: [nextui()],
