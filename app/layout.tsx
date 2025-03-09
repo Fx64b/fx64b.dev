4c0c693118a534870f0a25f4815f52cc569b18c6
Fx64b <90456762+Fx64b@users.noreply.github.com>
2025-03-09 13:21:05 +0100

    fix: issues from tailwind v4 upgrade * Add hr Tag between project cards * Add temporary workaround for link color

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={'bg-background dark min-h-fit overflow-x-hidden'}>
+            <body className={'bg-background text-foreground dark min-h-fit overflow-x-hidden'}>
                 <HeroUIProvider>
                     <Header />
                     {children}
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -6,6 +6,7 @@ import {
     CardHeader,
     Divider,
     Link,
+    Button,
 } from '@heroui/react'
 
 import Image from 'next/image'
@@ -18,36 +19,38 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ project }: ProjectCardProps) => {
     return (
-        <Link
-            href={project.link}
-            isExternal
-            className={'md:mx-auto md:max-w-(--breakpoint-md)'}
-        >
-            <Card>
-                <CardHeader className={'flex gap-4'}>
-                    <Image
-                        className={'rounded-md'}
-                        src={project.logo}
-                        alt={project.title + ' logo'}
-                        width={40}
-                        height={40}
-                    />
-                    <p className={'text-md'}>{project.title}</p>
-                    <Badge status={project.status} />
-                </CardHeader>
-                <Divider />
-                <CardBody className={'flex gap-4 md:min-w-[768px]'}>
-                    <p className={'whitespace-pre-wrap'}>
-                        {project.description}
-                    </p>
-                </CardBody>
-                <CardFooter>
-                    <Link isExternal showAnchorIcon href={project.githubLink}>
-                        View source code on GitHub.
-                    </Link>
-                </CardFooter>
-            </Card>
-        </Link>
+        <Card className="md:mx-auto md:max-w-(--breakpoint-md)">
+            <CardHeader className={'flex gap-4'}>
+                <Image
+                    className={'rounded-md'}
+                    src={project.logo}
+                    alt={project.title + ' logo'}
+                    width={40}
+                    height={40}
+                />
+                <p className={'text-md'}>{project.title}</p>
+                <Badge status={project.status} />
+            </CardHeader>
+            <Divider />
+            <CardBody className={'flex gap-4 md:min-w-[768px]'}>
+                <p className={'whitespace-pre-wrap'}>
+                    {project.description}
+                </p>
+            </CardBody>
+            <CardFooter className="flex justify-between">
+                <Link isExternal showAnchorIcon href={project.githubLink}>
+                    View source code on GitHub.
+                </Link>
+                <Button
+                    as={Link}
+                    href={project.link}
+                    isExternal
+                    variant="light"
+                >
+                    View Project
+                </Button>
+            </CardFooter>
+        </Card>
     )
 }
 
