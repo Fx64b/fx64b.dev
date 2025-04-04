73ae96656585d78d37e38a9ca7ebc92883e1059a
Fx64b <90456762+Fx64b@users.noreply.github.com>
2025-04-04 17:47:51 +0200

    feat(ci): improve coverage reporting and update test command - (hopefully it works, I have no idea what I'm doing)

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         css: false,
         coverage: {
             provider: 'v8',
-            reporter: ['text', 'json', 'lcov'],
+            reporter: ['text', 'json-summary', 'html'],
             include: ['components/tools/**'],
         },
     },
